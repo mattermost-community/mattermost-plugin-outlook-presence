ac13a7eb65842b3b187b2a34b7173d10ba76eb08
Manoj Malik <manoj@brightscout.com>
2022-04-04 19:56:17 +0530

    Added the logic to handle multiple websocket clients (#3)
    * Added the logic to handle multiple websocket clients
    Created a Pool struct for maintaining multiple clients and broadcasting messages to every client
    Created a Client struct for denoting every client
    Changed the Users type to Emails and changed the format for the response body of GetStatusesByEmails API
    
    * Review fixes
    Replaced fmt.Print with api.Log
    Removed the serializer.emails file

--- server/activate_hooks.go
+++ server/activate_hooks.go
@@ -1,11 +1,16 @@
 package main
 
+import "github.com/Brightscout/mattermost-plugin-outlook-presence/server/websocket"
+
 func (p *Plugin) OnActivate() error {
 	if err := p.OnConfigurationChange(); err != nil {
 		return err
 	}
 
 	// Initialize DB service
+	pool := websocket.NewPool()
+	go pool.Start(p.API)
+	p.wsPool = pool
 	p.router = p.InitAPI()
 	return nil
 }
--- server/api.go
+++ server/api.go
@@ -8,10 +8,9 @@ import (
 	"runtime/debug"
 
 	"github.com/Brightscout/mattermost-plugin-outlook-presence/server/serializer"
+	"github.com/Brightscout/mattermost-plugin-outlook-presence/server/websocket"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 	"github.com/mattermost/mattermost-server/v6/model"
-	"github.com/mattermost/mattermost-server/v6/plugin"
 )
 
 // InitAPI initializes the REST API
@@ -27,68 +26,50 @@ func (p *Plugin) InitAPI() *mux.Router {
 	// TODO: Remove the API below as it is unnecessary
 	s.HandleFunc("/status/{email}", p.GetStatusByEmail).Methods(http.MethodGet)
 	s.HandleFunc("/statuses", p.GetStatusesByEmails).Methods(http.MethodPost)
-	s.HandleFunc("/ws", p.HandleWebsocketConnection).Methods(http.MethodGet)
+	s.HandleFunc("/ws", p.serveWebSocket)
 
 	// 404 handler
 	r.Handle("{anything:.*}", http.NotFoundHandler())
 	return r
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  model.SocketMaxMessageSizeKb,
-	WriteBufferSize: model.SocketMaxMessageSizeKb,
-}
-
-func (p *Plugin) HandleWebsocketConnection(w http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
-	// upgrade this connection to a WebSocket
-	// connection
-	ws, err := upgrader.Upgrade(w, r, nil)
+func (p *Plugin) serveWebSocket(w http.ResponseWriter, r *http.Request) {
+	connection, err := websocket.CreateConnection(w, r)
 	if err != nil {
-		p.API.LogError("error in creating websocket", "Error", err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		p.API.LogError("error in creating websocket connection", "Error", err.Error())
 		return
 	}
 
-	p.ws = ws
-	reader(ws, p.API)
-}
-
-// a reader which listens for new messages being sent to our WebSocket endpoint
-func reader(conn *websocket.Conn, api plugin.API) {
-	for {
-		// read in a message
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			api.LogError(err.Error())
-			return
-		}
-
-		// write the same message back
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			api.LogError(err.Error())
-			return
-		}
-
+	client := &websocket.Client{
+		Conn: connection,
+		Pool: p.wsPool,
 	}
+
+	p.wsPool.Register <- client
+	client.Read(p.API)
 }
 
 func (p *Plugin) PublishStatusChanged(w http.ResponseWriter, r *http.Request) {
-	statusChangedEvent := serializer.StatusChangedEventFromJson(r.Body)
-	if err := statusChangedEvent.PrePublish(); err != nil {
+	statusChangedEvent, err := serializer.StatusChangedEventFromJson(r.Body)
+	if err != nil {
+		p.logAndReturnError(w, fmt.Sprintf("error in deserializing the request body. Error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	if err = statusChangedEvent.PrePublish(); err != nil {
 		p.logAndReturnError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	user, userErr := p.API.GetUser(statusChangedEvent.UserID)
 	if userErr != nil {
-		p.logAndReturnError(w, fmt.Sprintf("Unable to get user by id %s. Error: %v", statusChangedEvent.UserID, userErr.Error()), userErr.StatusCode)
+		p.logAndReturnError(w, fmt.Sprintf("Unable to get user by id %s. Error: %s", statusChangedEvent.UserID, userErr.Error()), userErr.StatusCode)
 		return
 	}
 
 	statusChangedEvent.Email = user.Email
-	p.ws.WriteJSON(statusChangedEvent)
-	returnStatusOK(w)
+	p.wsPool.Broadcast <- statusChangedEvent
+	writeStatusOK(w)
 }
 
 func (p *Plugin) GetStatusByEmail(w http.ResponseWriter, r *http.Request) {
@@ -101,20 +82,20 @@ func (p *Plugin) GetStatusByEmail(w http.ResponseWriter, r *http.Request) {
 
 	user, userErr := p.API.GetUserByEmail(email)
 	if userErr != nil {
-		p.logAndReturnError(w, fmt.Sprintf("Unable to get user with email %s. Error: %v", email, userErr.Error()), userErr.StatusCode)
+		p.logAndReturnError(w, fmt.Sprintf("Unable to get user with email %s. Error: %s", email, userErr.Error()), userErr.StatusCode)
 		return
 	}
 
 	status, err := p.API.GetUserStatus(user.Id)
 	if err != nil {
-		p.logAndReturnError(w, fmt.Sprintf("Unable to get user's status. Id: %s. Error: %v", user.Id, err.Error()), err.StatusCode)
+		p.logAndReturnError(w, fmt.Sprintf("Unable to get user's status. Id: %s. Error: %s", user.Id, err.Error()), err.StatusCode)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	response, respErr := status.ToJSON()
 	if respErr != nil {
-		p.logAndReturnError(w, fmt.Sprintf("Unable to convert user's status to JSON. Error: %v", respErr.Error()), http.StatusInternalServerError)
+		p.logAndReturnError(w, fmt.Sprintf("Unable to convert user's status to JSON. Error: %s", respErr.Error()), http.StatusInternalServerError)
 		return
 	}
 
@@ -124,9 +105,13 @@ func (p *Plugin) GetStatusByEmail(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Plugin) GetStatusesByEmails(w http.ResponseWriter, r *http.Request) {
-	users := serializer.UsersFromJson(r.Body)
-	userIds := make([]string, 0)
-	for _, email := range users.Emails {
+	var emails []string
+	if err := json.NewDecoder(r.Body).Decode(&emails); err != nil {
+		p.logAndReturnError(w, fmt.Sprintf("error in deserializing the request body. Error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+	userIds := make([]string, len(emails))
+	for _, email := range emails {
 		if !model.IsValidEmail(email) {
 			p.logAndReturnError(w, fmt.Sprintf("email %s is not valid", email), http.StatusBadRequest)
 			return
@@ -134,7 +119,7 @@ func (p *Plugin) GetStatusesByEmails(w http.ResponseWriter, r *http.Request) {
 
 		user, userErr := p.API.GetUserByEmail(email)
 		if userErr != nil {
-			p.logAndReturnError(w, fmt.Sprintf("Unable to get user with email %s. Error: %v", email, userErr.Error()), userErr.StatusCode)
+			p.logAndReturnError(w, fmt.Sprintf("Unable to get user with email %s. Error: %s", email, userErr.Error()), userErr.StatusCode)
 			return
 		}
 
@@ -143,14 +128,14 @@ func (p *Plugin) GetStatusesByEmails(w http.ResponseWriter, r *http.Request) {
 
 	statuses, err := p.API.GetUserStatusesByIds(userIds)
 	if err != nil {
-		p.logAndReturnError(w, fmt.Sprintf("Unable to get users' statuses. Error: %v", err.Error()), err.StatusCode)
+		p.logAndReturnError(w, fmt.Sprintf("Unable to get users' statuses. Error: %s", err.Error()), err.StatusCode)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	response, respErr := json.Marshal(statuses)
 	if respErr != nil {
-		p.logAndReturnError(w, fmt.Sprintf("Unable to convert users' statuses to JSON. Error: %v", respErr.Error()), http.StatusInternalServerError)
+		p.logAndReturnError(w, fmt.Sprintf("Unable to convert users' statuses to JSON. Error: %s", respErr.Error()), http.StatusInternalServerError)
 		return
 	}
 
@@ -159,10 +144,11 @@ func (p *Plugin) GetStatusesByEmails(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func returnStatusOK(w http.ResponseWriter) {
+func writeStatusOK(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
-	m := make(map[string]string)
-	m[model.STATUS] = model.StatusOk
+	m := map[string]string{
+		model.STATUS: model.StatusOk,
+	}
 	_, _ = w.Write([]byte(model.MapToJSON(m)))
 }
 
